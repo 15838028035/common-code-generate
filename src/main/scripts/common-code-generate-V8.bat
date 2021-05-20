0ff92d18c83e0ed1c893c304d310c5793cb931b3
15838028035 <15838028035@163.com>
2021-05-20 14:25:24 +0800

    添加V8

---
+++ src/main/scripts/common-code-generate-V8.bat
@@ -0,0 +1,15 @@
+
+@echo off & setlocal enabledelayedexpansion
+
+echo %JAVA_HOME%;
+
+set LIB_JARS=""
+cd ..\lib
+for %%i in (*) do set LIB_JARS=!LIB_JARS!;..\lib\%%i
+cd ..\scripts
+
+
+echo %LIB_JARS%;
+
+java -Djava.awt.headless=falsh  -classpath .;%LIB_JARS% com.lj.app.core.common.generator.GeneratorMainFrameV8  
+pause
